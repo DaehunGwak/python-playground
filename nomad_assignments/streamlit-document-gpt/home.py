7f337ff37d890332bf7fd3b32c28d5898f3b39f8
DaehunGwak <rhkrgns56@naver.com>
2024-09-04 01:05:45 +0900

    nomad gpt: 7. document gpt - 1. magic, write, selectbox

--- nomad_assignments/streamlit-document-gpt/home.py
+++ nomad_assignments/streamlit-document-gpt/home.py
@@ -1,10 +1,15 @@
 import streamlit as st
+from langchain_core.prompts import PromptTemplate
 
+st.write("hello")
+st.write([1, 2, 3, 4])
 
-st.title("Hello world!")
+{"x": 1}
+PromptTemplate
+p = PromptTemplate.from_template("xxxx")
+p # magic : write 호출 없이 그냥 이렇게 적는 것만으로 웹에 표시해줌 > 별로임 그냥 write 쓰자
 
-st.subheader("Welcome to streamlit!")
-
-st.markdown("""
-### I love it!
-""")
+st.selectbox(
+    label="Choose your model",
+    options=("GPT3", "GPT4")
+)
