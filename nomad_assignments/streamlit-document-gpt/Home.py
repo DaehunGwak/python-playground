cd0320a7e6f500e49fc8f362ce76a87ac560d859
DaehunGwak <rhkrgns56@naver.com>
2024-09-05 00:02:40 +0900

    nomad gpt: 7. document gpt - 4. set-up files

--- nomad_assignments/streamlit-document-gpt/Home.py
+++ nomad_assignments/streamlit-document-gpt/Home.py
@@ -1,29 +1,23 @@
 import streamlit as st
-from datetime import datetime
-
-# st.title("Sidebar Test")
-# st.write(datetime.now())
-
-# sidebar option 1
-# st.sidebar.title('sidebar title')
-# st.sidebar.write('write test')
-
-# with st.sidebar:
-#     st.title('sidebar title')
-#     st.write(datetime.now())
-#
-# tab_one, tab_two, tab_three = st.tabs(['A', 'B', 'C'])
-#
-# with tab_one:
-#     st.write('a')
-# with tab_two:
-#     st.write('b')
-# with tab_three:
-#     st.write('c')
 
 st.set_page_config(
     page_title="FullstackGPT Home",
     page_icon="🫠"
 )
 
-st.title("FullstackGPT Home")
+st.markdown(
+    """
+# Hello!
+
+Welcome to my FullstackGPT Portfolio!
+
+Here are the apps I made:
+
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
