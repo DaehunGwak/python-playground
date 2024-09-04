47485d16bf10bebe605288a30fc6774ac802286f
DaehunGwak <rhkrgns56@naver.com>
2024-09-05 00:24:27 +0900

    nomad gpt: 7. document gpt - 4. chat messages, session state

--- nomad_assignments/streamlit-document-gpt/pages/01_DocumentGPT.py
+++ nomad_assignments/streamlit-document-gpt/pages/01_DocumentGPT.py
@@ -1,3 +1,41 @@
 import streamlit as st
+import time
 
-st.title('Hello World!')
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+
+def draw_message(message, role):
+    with st.chat_message(role):
+        st.write(message)
+
+
+def save_message(message, role):
+    st.session_state["messages"].append({
+        "message": message,
+        "role": role,
+    })
+
+
+st.title("DocumentGPT")
+
+for _message in st.session_state["messages"]:
+    draw_message(_message['message'], _message['role'])
+
+input_message = st.chat_input("Send a message to the ai")
+
+if input_message:
+    draw_message(input_message, "human")
+    save_message(input_message, "human")
+    time.sleep(2)
+    draw_message(f"You said: {input_message}", "ai")
+    save_message(f"You said: {input_message}", "ai")
+
+# with st.status("Embeding file...", expanded=True) as status:
+#     time.sleep(3)
+#     st.write("Getting the file")
+#     time.sleep(3)
+#     st.write("Embedding the file")
+#     time.sleep(3)
+#     st.write("Caching the file")
+#     status.update(label="Error", state="error")
---
+++ nomad_assignments/streamlit-document-gpt/pages/07_Settings.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Your session state:")
+st.write(st.session_state)
