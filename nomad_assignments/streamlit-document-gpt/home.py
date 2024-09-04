01c28227706422586a1bebd883809f4ed3a90620
DaehunGwak <rhkrgns56@naver.com>
2024-09-04 23:00:02 +0900

    nomad gpt: 7. document gpt - 2. data flow

--- nomad_assignments/streamlit-document-gpt/home.py
+++ nomad_assignments/streamlit-document-gpt/home.py
@@ -1,15 +1,21 @@
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from datetime import datetime
 
-st.write("hello")
-st.write([1, 2, 3, 4])
+# 데이터가 변경될 때 마다 streamlit home.py 파일 전체가 다시 실행됨 흐음....
 
-{"x": 1}
-PromptTemplate
-p = PromptTemplate.from_template("xxxx")
-p # magic : write 호출 없이 그냥 이렇게 적는 것만으로 웹에 표시해줌 > 별로임 그냥 write 쓰자
+st.title(datetime.now())
 
-st.selectbox(
+model = st.selectbox(
     label="Choose your model",
     options=("GPT3", "GPT4")
 )
+if model == "GPT3":
+    st.write("cheap")
+else:
+    st.write("not cheap...")
+
+name = st.text_input("What is your name?")
+st.write(name)
+
+value = st.slider("temperature", min_value=0.0, max_value=1.0)
+st.write(f"temperature: {value}")
