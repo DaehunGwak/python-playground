d28288d4f29691d1d7d82aa165306aa285f0d5ee
DaehunGwak <rhkrgns56@naver.com>
2024-09-08 05:18:56 +0900

    nomad gpt: 9. quiz gpt - 3. Question Prompt

--- nomad_assignments/streamlit-document-gpt/pages/03_QuizGPT.py
+++ nomad_assignments/streamlit-document-gpt/pages/03_QuizGPT.py
@@ -8,6 +8,7 @@ from datetime import timedelta
 import streamlit as st
 from langchain_community.document_loaders import TextLoader
 from langchain_community.retrievers import WikipediaRetriever
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_text_splitters import CharacterTextSplitter
 from streamlit.runtime.scriptrunner_utils.script_run_context import get_script_run_ctx
@@ -34,12 +35,16 @@ def split_files(input_file, session_id):
     return loader.load_and_split(text_splitter=character_text_splitter)
 
 
+def format_docs(documents):
+    return "\n\n".join(document.page_content for document in documents)
+
+
 # states
 ctx = get_script_run_ctx()
 docs = None
 llm = ChatOpenAI(
     model_name="gpt-4o-mini",
-    teperature=0.1,
+    temperature=0.1,
 )
 
 # views
@@ -47,7 +52,6 @@ st.set_page_config(
     page_title="Quiz GPT",
     page_icon="🧐"
 )
-
 st.title("🧐 Quiz GPT")
 
 with st.sidebar:
@@ -68,7 +72,41 @@ with st.sidebar:
                 docs = retriever.get_relevant_documents(topic)
 
 if docs:
-    st.write(docs)
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", """
+You are a helpful assistant that is role playing as a teacher.
+     
+Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+
+Each question should have 4 answers, three of them must be incorrect and one should be correct.
+     
+Use (o) to signal the correct answer.
+     
+Question examples:
+     
+Question: What is the color of the ocean?
+Answers: Red|Yellow|Green|Blue(o)
+     
+Question: What is the capital or Georgia?
+Answers: Baku|Tbilisi(o)|Manila|Beirut
+     
+Question: When was Avatar released?
+Answers: 2007|2001|2009(o)|1998
+     
+Question: Who was Julius Caesar?
+Answers: A Roman Emperor(o)|Painter|Actor|Model
+     
+Your turn!
+     
+Context: {context}
+""")
+    ])
+    chain = {"context": format_docs} | prompt | llm
+
+    start = st.button("Generate Quiz")
+    if start:
+        results = chain.invoke(docs)
+        st.write(results.content.split("\n\n"))
 else:
     st.info("""
     I will make a quiz from Wikipedia articles of files you upload to test
