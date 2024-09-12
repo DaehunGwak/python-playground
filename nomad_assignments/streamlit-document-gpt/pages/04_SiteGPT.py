18751b8a6497b595a722d4b4fe35301e53cabcbc
DaehunGwak <rhkrgns56@naver.com>
2024-09-13 02:21:49 +0900

    nomad gpt: 10. site gpt - snap shot

--- nomad_assignments/streamlit-document-gpt/pages/04_SiteGPT.py
+++ nomad_assignments/streamlit-document-gpt/pages/04_SiteGPT.py
@@ -3,6 +3,7 @@
   - blog: https://openai.com/index/introducing-gpts/
   - sitemap: https://openai.com/sitemap.xml > 보안 이슈로 막힌 듯?
   - 과제 sitemap: https://developers.cloudflare.com/sitemap-0.xml
+  - 내 과거: https://daehungwak.github.io/til/sitemap.xml
 - troubleshooting
   - python SSL: https://coinpipe.tistory.com/171
   - user-agent issue: `poetry add fake_useragent`
@@ -12,7 +13,10 @@ from datetime import timedelta
 import streamlit as st
 from fake_useragent import UserAgent
 from langchain_community.document_loaders import SitemapLoader
+from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
+from langchain_core.vectorstores import VectorStoreRetriever
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 user_agent = UserAgent()
@@ -37,15 +41,27 @@ def parse_page(soup):
     ttl=timedelta(hours=12),
 )
 def load_website(target_url: str) -> list[Document]:
-    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    _splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         model_name="gpt-4o-mini",
         chunk_size=1000,
-        chunk_overlap=200,
+        chunk_overlap=100,
     )
-    loader = SitemapLoader(target_url, parsing_function=parse_page)
-    loader.requests_per_second = 2
-    loader.header = {'User-Agent': user_agent.random}
-    return loader.load_and_split(text_splitter=splitter)
+    _loader = SitemapLoader(target_url, parsing_function=parse_page)
+    _loader.requests_per_second = 2
+    _loader.header = {'User-Agent': user_agent.random}
+    return _loader.load_and_split(text_splitter=_splitter)
+
+
+@st.cache_resource(
+    show_spinner="로드한 sitemap 문서들을 임베딩하고 있습니다 👩🏻‍🔧",
+    ttl=timedelta(hours=12),
+)
+def get_retriever(target_url: str, _target_docs: list[Document]) -> VectorStoreRetriever:
+    _vector_store = FAISS.from_documents(
+        documents=_target_docs,
+        embedding=GPT4AllEmbeddings()
+    )
+    return _vector_store.as_retriever()
 
 
 st.set_page_config(
@@ -69,4 +85,6 @@ if url:
             st.error("Please write down a Sitemap URL")
     else:
         docs = load_website(url)
-        st.write(docs)
+        retriever = get_retriever(url, docs)
+        result_docs = retriever.invoke('클린 코드가 뭐야?')
+        st.write(result_docs)
