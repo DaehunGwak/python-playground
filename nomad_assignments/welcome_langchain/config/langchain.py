64fe5c177dc4539bde0d6b651c7984380415f048
DaehunGwak <rhkrgns56@naver.com>
2024-09-16 01:26:17 +0900

    nomad gpt: 14. openai assist - 1. default functions

--- nomad_assignments/welcome_langchain/config/langchain.py
+++ nomad_assignments/welcome_langchain/config/langchain.py
@@ -4,10 +4,11 @@ import json
 KEY_FILE_NAME = 'api-key.json'
 
 
-def setup_langchain_openai_envs():
-    with open(KEY_FILE_NAME) as key_file:
+def setup_langchain_openai_envs(path: str = "./"):
+    file_path = os.path.join(path, KEY_FILE_NAME)
+    with open(file_path) as key_file:
         keys = json.load(key_file)
 
     os.environ['LANGCHAIN_TRACING_V2'] = 'true'
     os.environ['LANGCHAIN_API_KEY'] = keys['langchain-personal-key']
-    os.environ['OPENAI_API_KEY'] = keys['openai-key']
+    os.environ['OPENAI_API_KEY'] = keys['openai_note-key']
