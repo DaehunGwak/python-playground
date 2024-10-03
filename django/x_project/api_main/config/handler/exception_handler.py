55d66832773faf10bc019c4ffe2c19d75fafeae6
DaehunGwak <rhkrgns56@naver.com>
2024-10-04 01:34:46 +0900

    nomad: anb hw - add drf & custrom error handler & n+1 problem

---
+++ django/x_project/api_main/config/handler/exception_handler.py
@@ -0,0 +1,30 @@
+import logging
+
+from django.http import JsonResponse, Http404
+from rest_framework import status
+from rest_framework.views import exception_handler
+
+_logger = logging.getLogger(__name__)
+
+
+def _not_found_handler(exc, context):
+    _logger.warning(f"Not Found.\nexc: {exc.__class__}, {exc}\ncontext: {context}")
+    return JsonResponse({'detail': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+def _server_error_handler(exc, context):
+    _logger.error(f"Internal Server Error.\nexc: {exc.__class__}, {exc}\ncontext: {context}")
+    return JsonResponse({'detail': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+def global_exception_handler(exc, context):
+    if isinstance(exc, Http404):
+        return _not_found_handler(exc, context)
+
+    response = exception_handler(exc, context)
+
+    if response is not None:
+        return _server_error_handler(exc, context)
+
+    _logger.error(f'Not Defined Error.\nexc: {exc.__class__}, {exc}\ncontext: {context}')
+    return response
--- django/x_project/api_main/response/tweet.py
+++ django/x_project/api_main/response/tweet.py
@@ -1,5 +1,7 @@
 from typing import Dict, Any
 
+from rest_framework import serializers
+
 from data_tweets.models import Tweet
 
 
@@ -12,3 +14,17 @@ def to_tweet_response(tweet: Tweet) -> Dict[str, Any]:
         'created_at': str(tweet.created_at),
         'updated_at': str(tweet.updated_at),
     }
+
+
+class TweetUserResponse(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(read_only=True)
+
+
+class TweetResponse(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    user = TweetUserResponse(read_only=True, many=False)
+    payload = serializers.CharField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
